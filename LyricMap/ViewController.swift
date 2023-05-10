28b8afdaac3e65db3c6be76b66912c68e1f77d0a
iamStephenFang <Fangxuanmiao@hotmail.com>
2023-05-10 19:56:40 +0800

    Initial Commit

---
+++ LyricMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LyricMap
+//
+//  Created by StephenFang on 2023/5/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
